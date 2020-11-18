f8c88043a820f53ecc6a4eb2477eb5ba611699e8
shahakash28 <akashshah08@outlook.com>
2020-11-18 11:52:59 +0530

    test file added

---
+++ psi_analytics_eurocrypt19/tests/test_circuit.cpp
@@ -0,0 +1,85 @@
+#include "MPCHonestMajority/MPSI_Party.h"
+#include "MPCHonestMajority/ZpKaratsubaElement.h"
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+void circuit_thread(int size, char** circuitArgv, vector<uint64_t>& bins, int nbins) {
+    MPSI_Party<ZpMersenneLongElement> mpsi(size, circuitArgv, bins, nbins);
+    cout<<"End of Thread Call"<<endl;
+    //sleep(5);
+    //mpsi.runMPSI();
+}
+
+void prepareArgs(char** circuitArgv, uint32_t role, uint64_t np, uint64_t nbins,
+                 string outputFileName, string circuitFileName, string partiesFile) {
+  circuitArgv[0] = "./build/MPCHonestMajority";
+  circuitArgv[1] = "-partyID";
+  sprintf(circuitArgv[2], "%lu", role);
+  circuitArgv[3] = "-partiesNumber";
+  sprintf(circuitArgv[4], "%llu", np);
+  circuitArgv[5] = "-numBins";
+  sprintf(circuitArgv[6], "%llu", nbins);
+  circuitArgv[7] = "-inputsFile";
+  string arg_val = "../in_party_" + to_string(role) + ".txt";
+  sprintf(circuitArgv[8], arg_val.c_str());
+  circuitArgv[9] = "-outputsFile";
+  strcpy(circuitArgv[10], outputFileName.c_str());
+  circuitArgv[11] = "-circuitFile";
+  strcpy(circuitArgv[12], circuitFileName.c_str());
+  circuitArgv[13] = "-fieldType";
+  circuitArgv[14] = "ZpMersenne61";
+  circuitArgv[15] = "-genRandomSharesType";
+  circuitArgv[16] = "HIM";
+  circuitArgv[17] = "-multType";
+  circuitArgv[18] = "DN";
+  circuitArgv[19] = "-verifyType";
+  circuitArgv[20] = "Single";
+  circuitArgv[21] = "-partiesFile";
+  strcpy(circuitArgv[22], partiesFile.c_str());
+  circuitArgv[23] = "-internalIterationsNumber";
+  circuitArgv[24] = "1";
+}
+
+
+int main(int argc, char** argv) {
+  //cout<<"It Works!"<<endl;
+  uint64_t np = atoi(argv[1]);
+  uint64_t nbins = atoi(argv[2]);
+
+  int size[np];
+  char** circuitArgv[np];
+
+  vector<uint64_t> bins[np];
+  uint64_t val;
+  for(uint32_t i=0; i<np; i++) {
+    size[i] = 25;
+    circuitArgv[i] = (char **) malloc(sizeof(char*)*(size[i]));
+
+    for(int j=0; j < size[i]; j++) {
+      circuitArgv[i][j] = (char *) malloc(sizeof(char)*50);
+    }
+    prepareArgs(circuitArgv[i], i, np, nbins, string(argv[3]), string(argv[4]), string(argv[5]));
+    ifstream myfile;
+    string myInputFile = "../in_party_" + to_string(i) + ".txt";
+    myfile.open(myInputFile);
+    for(int j=0; j<nbins; j++) {
+      myfile >> val;
+      bins[i].push_back(val);
+    }
+  }
+
+  std::thread cmp_threads[np];
+
+  for(int i=0;i<np;i++) {
+    cmp_threads[i] = std::thread(circuit_thread, size[i], std::ref(circuitArgv[i]), std::ref(bins[i]), nbins);
+  }
+
+  for(int i=0; i<np; i++) {
+    cmp_threads[i].join();
+  }
+
+  return 0;
+}
