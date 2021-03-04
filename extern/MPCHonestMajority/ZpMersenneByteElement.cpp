a04c84c7cb7cd3f7d6dfdc970108d07b884efa2a
Nishka Dasgupta <nishka.dasgupta@yahoo.com>
2021-03-04 11:41:40 +0530

    Multiplication
    Multiplication in fields of Mersenne primes less than 8 bits in size.
    
    Signed-off-by: Nishka Dasgupta <nishka.dasgupta@yahoo.com>

--- extern/MPCHonestMajority/ZpMersenneByteElement.cpp
+++ extern/MPCHonestMajority/ZpMersenneByteElement.cpp
@@ -1,7 +1,8 @@
 #include "ZpMersenneByteElement.h"
 #include "gmp.h"
 
-static uint8_t p = 31;
+static uint8_t p = 31; // Mersenne Prime only; multiplication protocol does not work with non-Mersenne primes
+static uint8_t p_size = 5; // size of bit representation of p
 
 ZpMersenneByteElement::ZpMersenneByteElement() {
 	this->elem = 0;
@@ -66,5 +67,31 @@ ZpMersenneByteElement& ZpMersenneByteElement::operator-=(const ZpMersenneByteEle
 		ans = ans + p;
 	}
 	this->elem = (uint8_t) ans;
+	return *this;
+}
+
+ZpMersenneByteElement ZpMersenneByteElement::operator*(const ZpMersenneByteElement& other) {
+	ZpMersenneByteElement answer;
+	
+	uint16_t prod = (uint16_t)this->elem * (uint16_t)other.elem;
+	uint8_t bottom = prod & p;
+	uint8_t top = (prod >> p_size);
+	uint8_t ans = top + bottom;
+	if(ans >= p) {
+		ans = ans - p;
+	}
+	answer.elem = ans;
+	return answer;
+}
+
+ZpMersenneByteElement& ZpMersenneByteElement::operator*=(const ZpMersenneByteElement& other) {
+	uint16_t prod = (uint16_t)this->elem * (uint16_t)other.elem;
+	uint8_t bottom = prod & p;
+	uint8_t top = (prod >> p_size);
+	uint8_t ans = top + bottom;
+	if(ans >= p) {
+		ans = ans - p;
+	}
+	this->elem = ans;
 	return *this;
 }
